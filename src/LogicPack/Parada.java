ac1681bef01c35629529797729f89817ab78d547
Yodariz <ynmr0001@ce.pucmm.edu.do>
2026-02-27 19:57:04 -0400

    Clase Parada con atributos y metodos

---
+++ src/LogicPack/Parada.java
@@ -0,0 +1,67 @@
+package LogicPack;
+
+public class Parada {
+    private String id;
+    private String nombre;
+    private double coordX;
+    private double coordY;
+
+    public Parada(String id, String nombre, double coordX, double coordY) {
+        this.id = id;
+        this.nombre = nombre;
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    public String getId() {
+        return id;
+    }
+// Conveniente no tener
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCoordX() {
+        return coordX;
+    }
+    public void setCoordX(double coordX) {
+        this.coordX = coordX;
+    }
+
+    public double getCoordY() {
+        return coordY;
+    }
+    public void setCoordY(double coordY) {
+        this.coordY = coordY;
+    }
+
+
+    //----------------- METODOS -----------------
+
+    /*
+    Compara esta parada con otro objeto y determina si son iguales.
+    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Parada parada = (Parada) o;
+        return id.equals(parada.id);
+    }
+
+    /*
+    Genera un codigo que esta asociado con la parada
+     */
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id);
+    }
+}
+
