53d6ccc3cfdb9283e38104e0275be92730cf968f
A1000erG <AXMG0002@ce.pucmm.edu.do>
2026-02-28 19:29:36 -0400

    Clases de Algoritmos, Ruta y otros métodos

--- src/LogicPack/Algorithms/BellmanFordAlgo.java
+++ src/LogicPack/Algorithms/BellmanFordAlgo.java
@@ -1,4 +1,13 @@
 package LogicPack.Algorithms;
 
+import LogicPack.GrafoTransporte;
+import LogicPack.Pond;
+import java.util.List;
+
 public class BellmanFordAlgo implements Algozed{
+    @Override
+    public List buscarRutaOptima(GrafoTransporte grafo, Object origen, Object destino, Pond criterio) {
+        //Implementación
+        return List.of();
+    }
 }
--- src/LogicPack/Algorithms/FloydWarshallAlgo.java
+++ src/LogicPack/Algorithms/FloydWarshallAlgo.java
@@ -1,4 +1,14 @@
 package LogicPack.Algorithms;
 
+import LogicPack.GrafoTransporte;
+import LogicPack.Pond;
+
+import java.util.List;
+
 public class FloydWarshallAlgo implements Algozed{
+    @Override
+    public List buscarRutaOptima(GrafoTransporte grafo, Object origen, Object destino, Pond criterio) {
+        //IMplementación
+        return List.of();
+    }
 }
